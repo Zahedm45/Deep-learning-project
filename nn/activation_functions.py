82b474862993d9cb1cc3b64b0e6e2c89bf8a26ab
Zahed <zahedm45@gmail.com>
2025-11-16 18:07:32 +0100

    Added different activation functions for hidden layers and softmax for output layer.

---
+++ nn/activation_functions.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+
+
+
+def rectified_linea_unit(x):
+    return np.maximum(0, x)
+
+def rectified_linea_unit_derivative(x):
+    return (x > 0).astype(float)
+
+
+def hyperbolic_tan(x):
+    return np.tanh(x)
+
+def hyperbolic_tan_derivative(x):
+    return 1 - np.tanh(x) ** 2
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def softmax(z):
+    z = z - np.max(z, axis=1, keepdims=True)
+    exp_z = np.exp(z)
+    return exp_z / np.sum(exp_z, axis=1, keepdims=True)
