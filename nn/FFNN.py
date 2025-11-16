dfa26e596e1af1ac20c862bdfdd7b3a923cb3bce
Zahed <zahedm45@gmail.com>
2025-11-16 21:07:34 +0100

    Working on Feed-Forward method.

--- nn/FFNN.py
+++ nn/FFNN.py
@@ -1,4 +1,4 @@
-from nn.activation_functions import activations_map
+from nn.activation_functions import activations_map, softmax
 import numpy as np
 from jaxtyping import Float
 from nn.optimizers import Adam, SGD
@@ -22,9 +22,10 @@ class FFNN:
         epochs: int = 20,
         seed: int = 42,
     ):
+        self.cache = {}
         np.random.seed(seed)
 
-        self.num_hidden_layers = num_hidden_layers
+        self.num_hid_layers = num_hidden_layers
         self.hidden_units = hidden_units
         self.activation_name = activation
         self.weight_init = weight_init
@@ -53,9 +54,43 @@ class FFNN:
         self.params = {}
         for i in range(len(layer_dims) - 1):
             self.params[f"W{i+1}"] = init_weights(layer_dims[i], layer_dims[i + 1], method=weight_init)
-            self.params[f"b{i+1}"] = np.zeros((1, layer_dims[i+1]))
+            self.params[f"b{i+1}"] = np.zeros((1, layer_dims[i+1])) # bias b
 
         print(self.params)
+
+
+
+    def feed_forward(self, X: np.ndarray) -> np.ndarray:
+        self.cache = {"A0": X}
+
+        # Hidden layers
+        for i in range(1, self.num_hid_layers + 1):
+            A_prev = self.cache[f"A{i - 1}"]  # previous layer output
+            W = self.params[f"W{i}"]  # weight matrix for this layer
+            b = self.params[f"b{i}"]  # bias vector for this layer
+            weighted_sum = np.matmul(A_prev, W)  # matrix multiplication
+            Z = weighted_sum + b
+
+            self.cache[f"Z{i}"] = Z
+            self.cache[f"A{i}"] = self.activation(Z)
+
+        # Output layer
+        A_prev = self.cache[f"A{self.num_hid_layers}"]  # previous layer activation
+        W_out = self.params[f"W{self.num_hid_layers + 1}"]  # output layer weight matrix
+        b_out = self.params[f"b{self.num_hid_layers + 1}"]  # Output layer bias vector
+
+        weighted_sum_out = np.matmul(A_prev, W_out)  # matrix multiplication
+        Z_out = weighted_sum_out + b_out
+
+        self.cache[f"Z{self.num_hid_layers + 1}"] = Z_out
+        A_out = softmax(Z_out)
+        self.cache[f"A{self.num_hid_layers + 1}"] = A_out
+        return A_out
+
+
+
+
+
 
 
 
