ba41f9b8f8882684f7657b5b24ac3c5c9fddc847
Zahed <zahedm45@gmail.com>
2025-11-17 12:10:21 +0100

    Implemented methods train and evaluate (not completely done yet).

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import run
 from nn.FFNN import FFNN
 from utils import load_fashion_mnist_torch
 
 if __name__ == "__main__":
     (X_train, y_train), (X_val, y_val), (X_test, y_test) = load_fashion_mnist_torch()
 
-    ffnn_model = FFNN(num_features=784, num_output=10, num_hidden_layers=2, hidden_units=128,
+    ffnn_model = FFNN(n_features=784, n_output_ne=10, n_hid_layers=2, n_hid_neurons=128,
                       activation="relu", weight_init="he", optimizer="adam", learning_rate=0.001,
                       l2_coeff=1e-4, batch_size=128, epochs=10
                       )
+
+    run.train(ffnn_model, X_train, y_train, X_val, y_val)
+
+    run.evaluate(ffnn_model, X_test, y_test)
---
+++ run.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+def iter_batches(X, y, batch_size=128, shuffle=True):
+
+    # Shuffles the indices
+    indices: np.ndarray = np.arange(X.shape[0])
+    if shuffle:
+        np.random.shuffle(indices)
+
+    # goes from 0 to number of images in X, stepping by 128 each time
+    for start_idx in range(0, X.shape[0], batch_size):
+        end_idx = min(start_idx + batch_size, X.shape[0])
+        chunk = indices[start_idx: end_idx]
+        yield X[chunk], y[chunk]
+
+def compute_accuracy(y_pred, y_true):
+    return np.mean(y_pred == y_true)
+
+
+def train(model, X_train: np.ndarray, y_train: np.ndarray,
+          X_val: np.ndarray, y_val: np.ndarray, patience=2):
+    best_val_acc = 0.0
+    no_impr_counter = 0
+
+    for epoch in range(model.epochs):
+        losses = []
+        for X_batch, y_batch in iter_batches(X_train, y_train, model.batch_size):
+            y_pred = model.feed_forward(X_batch)
+            loss = model.compute_loss(y_pred, y_batch)
+            losses.append(loss)
+            model.backpropagate(y_batch)
+
+
+        y_val_pred = model.predict(X_val)
+        val_acc = compute_accuracy(y_val_pred, np.argmax(y_val, axis=1))
+
+        print(f"Epoch {epoch+1:02d}/{model.epochs} | "
+              f"Loss: {np.mean(losses):.3f} | Val Acc: {val_acc:.3f}")
+
+        # Early stop to avoid overfitting
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            no_impr_counter = 0
+        else:
+            no_impr_counter += 1
+
+        if no_impr_counter >= patience:
+            print("Early stopping at epoch " + str(epoch+1))
+            break
+
+
+def evaluate(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    val_acc = compute_accuracy(y_pred, np.argmax(y_test, axis=1))
+    print(f"Test Accuracy: {val_acc:.3f}")
+
+
